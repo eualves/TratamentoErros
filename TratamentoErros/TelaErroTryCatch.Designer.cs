1083d6bd9349df3f89d1d07b92b77e09954c799f
Leonardo <leoalvesbico@gmail.com>
2021-09-29 11:54:33 -0300

    Adicionar arquivos de projeto.

---
+++ TratamentoErros/TelaErroTryCatch.Designer.cs
@@ -0,0 +1,90 @@
+﻿
+namespace TratamentoErros
+{
+    partial class TelaErroTryCatch
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(159, 152);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(140, 43);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Erro Tray Catch";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.BackColor = System.Drawing.Color.Silver;
+            this.lblResultado.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblResultado.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblResultado.Location = new System.Drawing.Point(159, 9);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(287, 113);
+            this.lblResultado.TabIndex = 1;
+            this.lblResultado.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(318, 152);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(128, 43);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Throw";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // TelaErroTryCatch
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MediumSeaGreen;
+            this.ClientSize = new System.Drawing.Size(643, 298);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.button1);
+            this.Name = "TelaErroTryCatch";
+            this.Text = "Tratamento de Erro :: Try Catch";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ TratamentoErros/TelaErroTryCatch.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TratamentoErros
+{
+    public partial class TelaErroTryCatch : Form
+    {
+        public TelaErroTryCatch()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int n1 = 10;
+                int n2 = 2;
+
+                int resul = n1 / n2;
+
+                lblResultado.Text = string.Format("{0}+{1}={2}", n1.ToString(), n2.ToString(), resul.ToString());
+            }
+            catch(DivideByZeroException ex)
+            {
+                MessageBox.Show(ex.Message,"Alerta de erro", MessageBoxButtons.OK,MessageBoxIcon.Error);
+            }
+            catch
+            {
+                MessageBox.Show("Erro ao tentar executar o programa", "Alerta de erro");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int n1 = 80;
+                int n2 = 10;
+
+                if (n2 == 10)
+                {
+                    throw new Exception("Divisão por zero não pode!!!");
+                }
+                int resul = n1 / n2;
+                lblResultado.Text = string.Format("{0}+{1}={2}", n1.ToString(), n2.ToString(), resul.ToString());
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Atenção");
+            }
+            finally
+            {
+                MessageBox.Show("Sempre passa por aqui!");
+            }
+        }
+    }
+}
